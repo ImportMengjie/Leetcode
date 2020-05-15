55c099cec5727da43497db870e5cd448e82357de
ImportMengjie <limengjie@hotmail.com>
2020-05-15 12:04:49 +0800

    Bali Ha'i

---
+++ 195.Tenth-Line.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+:<<EOF
+Given a text file file.txt, print just the 10th line of the file.
+
+Example:
+
+Assume that file.txt has the following content:
+
+Line 1
+Line 2
+Line 3
+Line 4
+Line 5
+Line 6
+Line 7
+Line 8
+Line 9
+Line 10
+Your script should output the tenth line, which is:
+
+Line 10
+Note:
+1. If the file contains less than 10 lines, what should you output?
+2. There's at least three different solutions. Try to explore all possibilities.
+EOF
+
+line=`cat file.txt | wc -l`
+if [ $line -lt 10 ]; then
+    echo "The number of row is less than 10"
+else
+    awk '{if(NR==10){print $0}}' file.txt
+fi
