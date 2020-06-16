516bb09f02c332fbb789665de1344c0f1568a11d
ImportMengjie <limengjie@hotmail.com>
2020-06-16 23:04:16 +0800

    Something Beautiful

---
+++ 337.打家劫舍-iii.cpp
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=337 lang=cpp
+ *
+ * [337] 打家劫舍 III
+ *
+ * https://leetcode-cn.com/problems/house-robber-iii/description/
+ *
+ * algorithms
+ * Medium (57.18%)
+ * Likes:    376
+ * Dislikes: 0
+ * Total Accepted:    32.7K
+ * Total Submissions: 57K
+ * Testcase Example:  '[3,2,3,null,3,null,1]'
+ *
+ * 在上次打劫完一条街道之后和一圈房屋后，小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为“根”。
+ * 除了“根”之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。
+ * 如果两个直接相连的房子在同一天晚上被打劫，房屋将自动报警。
+ * 
+ * 计算在不触动警报的情况下，小偷一晚能够盗取的最高金额。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [3,2,3,null,3,null,1]
+ * 
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  2   3
+ * ⁠   \   \ 
+ * ⁠    3   1
+ * 
+ * 输出: 7 
+ * 解释: 小偷一晚能够盗取的最高金额 = 3 + 3 + 1 = 7.
+ * 
+ * 示例 2:
+ * 
+ * 输入: [3,4,5,1,3,null,1]
+ * 
+ * 3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \   \ 
+ * ⁠1   3   1
+ * 
+ * 输出: 9
+ * 解释: 小偷一晚能够盗取的最高金额 = 4 + 5 = 9.
+ * 
+ * 
+ */
+#define NULL 0
+#include <math.h>
+#include <array>
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+// @lc code=start
+class Solution {
+public:
+    int rob(TreeNode* root) {
+        auto&& ret = rob_(root);
+        return max(ret[0],ret[1]);
+    }
+
+    std::array<int,2> rob_(TreeNode* root){
+        std::array<int,2> ret{0,0};
+        if(!root) return ret;
+        auto&& left = rob_(root->left);
+        auto&& right = rob_(root->right);
+        ret[0] = max(left[0],left[1])+max(right[0],right[1]);
+        ret[1] = left[0]+right[0]+root->val;
+
+        return ret;
+    }
+    
+};
+// @lc code=end
+
